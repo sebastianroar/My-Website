99e38608b7aeac4ee8afc2a47d7b8d6d8e2200fa
Sebastian Roa <809sebas@gmail.com>
2023-03-28 20:42:55 -0500

    Web page blueprint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,22 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import Image from "next/image";
+import deved from "../../public/dev-ed-wave.png";
+import design from "../../public/design.png";
+import code from "../../public/code.png";
+import consulting from "../../public/consulting.png";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <>
       <Head>
@@ -9,7 +25,163 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className="bg-white px-10 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Welcome 👋</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:tex-6xl">
+              Sebastian Roa
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl">Front-End Developer</h3>
+            <p className="text-medium py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit maiores error distinctio saepe sequi! Beatae ipsam
+              voluptates exercitationem quasi aspernatur.
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image
+              src={deved}
+              layout="fill"
+              objectFit="cover"
+              alt="Deved Image"
+            />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className=" text-3xl py-1">Services I offer</h3>
+            <p className="text-medium py-5 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <span className="text-teal-500"> Reprehenderit</span> maiores
+              error distinctio saepe sequi! Beatae ipsam voluptates
+              exercitationem quasi aspernatur.
+            </p>
+            <p className="text-medium py-5 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <span className="text-teal-500"> Reprehenderit</span> maiores
+              error distinctio saepe sequi! Beatae ipsam voluptates
+              exercitationem quasi aspernatur.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                className="m-auto"
+                src={design}
+                width={100}
+                height={100}
+                alt="Design Image"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+                voluptates minima ad sed expedita? Deleniti officia numquam
+                itaque dicta.
+              </p>
+              <h4 className="py-4 text-teal-600">Technologies I use</h4>
+              <p className="text-gray-800 py-1">React</p>
+              <p className="text-gray-800 py-1">JavaScript</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                className="m-auto"
+                src={code}
+                width={100}
+                height={100}
+                alt="Design Image"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+                voluptates minima ad sed expedita? Deleniti officia numquam
+                itaque dicta.
+              </p>
+              <h4 className="py-4 text-teal-600">Technologies I use</h4>
+              <p className="text-gray-800 py-1">React</p>
+              <p className="text-gray-800 py-1">JavaScript</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                className="m-auto"
+                src={consulting}
+                width={100}
+                height={100}
+                alt="Design Image"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+                voluptates minima ad sed expedita? Deleniti officia numquam
+                itaque dicta.
+              </p>
+              <h4 className="py-4 text-teal-600">Technologies I use</h4>
+              <p className="text-gray-800 py-1">React</p>
+              <p className="text-gray-800 py-1">JavaScript</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className=" text-3xl py-1">Portfolio</h3>
+            <p className="text-medium py-5 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <span className="text-teal-500"> Reprehenderit</span> maiores
+              error distinctio saepe sequi! Beatae ipsam voluptates
+              exercitationem quasi aspernatur.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web1}
+                alt="Project Image"
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                src={web2}
+                alt="Project Image"
+                layout="responsive"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
